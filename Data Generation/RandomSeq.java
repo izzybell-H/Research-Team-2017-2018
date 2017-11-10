06247a89d8ff1d7ffe8a00b749dfc88d4be9cc05
izzybell-H <33534063+izzybell-H@users.noreply.github.com>
2017-11-10 15:44:43 -0500

    Update RandomSeq.java
    public static String randomSeq(String peptides, int length)
    {
      //Declares Variables
    	Random rand =  new Random();
    	String sequence = "";
    		
    	for(int j=0; j<length; j++)
    	{
    	  sequence += (char)(rand.nextInt(24)+65);
    	}
    	return sequence;
    }

--- Data Generation/RandomSeq.java
+++ Data Generation/RandomSeq.java
@@ -1,12 +1,26 @@
-public static String randomSeq(String peptides, int length)
+package peptideGeneration;
+
+//NAME              	Isabel Holtan
+//LAST MODIFIED     	10 November 2017
+//Class			Peptide Generation
+//DESCRIPTION       	Generates a random sequence of proteins and
+//			then divides into peptides and outputs to a file
+
+import java.util.*;
+
+public class PeptideGeneration
 {
-  //Declares Variables
-	Random rand =  new Random();
-	String sequence = "";
-		
-	for(int j=0; j<length; j++)
+	public String randomSeq(String peptides, int length)
 	{
-	  sequence += (char)(rand.nextInt(24)+65);
+		//Declares Variables
+		Random rand =  new Random();
+		String sequence = "";
+		
+		for(int j=0; j<length; j++)
+		{
+			sequence += peptides.charAt((rand.nextInt(peptides.length())));
+		}
+		
+		return sequence;
 	}
-	return sequence;
 }
