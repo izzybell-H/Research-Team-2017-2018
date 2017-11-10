6b064778931ca791b3d2bdb17157010dfea509e5
izzybell-H <33534063+izzybell-H@users.noreply.github.com>
2017-11-10 13:13:23 -0500

    Update randomSeq.java

--- randomSeq.java
+++ randomSeq.java
@@ -1,13 +1,13 @@
 public static String randomSeq(String peptides, int length)
+{
+	//Declares Variables
+	Random rand =  new Random();
+	String sequence = "";
+		
+	for(int j=0; j<length; j++)
 	{
-		//Declares Variables
-		Random rand =  new Random();
-		String sequence = "";
-		
-		for(int j=0; j<length; j++)
-		{
-			sequence += (char)(rand.nextInt(24)+65);
-		}
-		
-		return sequence;
+		sequence += (char)(rand.nextInt(24)+65);
 	}
+		
+	return sequence;
+}
