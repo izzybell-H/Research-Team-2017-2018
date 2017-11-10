0ccecb37784bc3d0837389e34b37b9896aac8356
izzybell-H <33534063+izzybell-H@users.noreply.github.com>
2017-11-10 13:17:14 -0500

    Create RandomSeq.java

---
+++ Data Generation/RandomSeq.java
@@ -0,0 +1,12 @@
+public static String randomSeq(String peptides, int length)
+{
+  //Declares Variables
+	Random rand =  new Random();
+	String sequence = "";
+		
+	for(int j=0; j<length; j++)
+	{
+	  sequence += (char)(rand.nextInt(24)+65);
+	}
+	return sequence;
+}
